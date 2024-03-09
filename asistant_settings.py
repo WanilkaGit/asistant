fab6aaf6e571f4bc19e9830bb8b6e16cb7965318
WanilkaGit <mahlunivan27@gmail.com>
2024-03-09 18:51:56 +0200

    dfgh

---
+++ asistant_settings.py
@@ -0,0 +1,44 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.switch import Switch
+
+class SettigsScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.grid_line = GridLayout(rows=1)
+        self.grid_line.bind(minimum_width = self.grid_line.setter("width"))
+        scrol = ScrollView()
+        scrol.add_widget(self.grid_line)
+
+        self.standart_btn = Button(text="Standart")
+        self.notes_btn = Button(text="Notes")
+        self.random_btn = Button(text="Random")
+        self.foto_editor_btn = Button(text="Foto editor")
+        self.calc_btn = Button(text="Calculator")
+        self.me_btn = Button(text="Me")
+        self.menu_btn = Button(text="Menu")
+
+        self.grid_line.add_widget(self.standart_btn)
+        self.grid_line.add_widget(self.notes_btn)
+        self.grid_line.add_widget(self.random_btn)
+        self.grid_line.add_widget(self.foto_editor_btn)
+        self.grid_line.add_widget(self.calc_btn)
+        self.grid_line.add_widget(self.me_btn)
+        self.grid_line.add_widget(self.menu_btn)
+        self.add_widget(scrol)
+
+class SettingsApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(SettigsScreen(name='menu'))
+        return sm
+
+if __name__ == "__main__":
+    SettingsApp().run()
