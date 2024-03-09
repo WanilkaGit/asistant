2f0b0e519c202f6048ddad2d87f6a74cd143d4de
WanilkaGit <mahlunivan27@gmail.com>
2024-03-09 15:03:31 +0200

    start asistant_settings

--- asistant_settings.py
+++ asistant_settings.py
@@ -2,13 +2,32 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 
 from kivy.uix.switch import Switch
+from kivy.uix.button import Button
 
-class NotesScreen(Screen):# тут прописаний перший клас/екран
-    a = Settings()
-    a.add_kivy_panel(SettingColor())
-    main = BoxLayout()
-    main.add_widget(a)
-    
+class SettingsScreen(Screen):
+    def __init__(self, **kwargs):# тут можна писати скільки хочеш аргументів
+        super().__init__(**kwargs)# копіюємо кістяк супер класу також
+        main_col = BoxLayout()
+        scrol = ScrollView()
+        grid_line = GridLayout(rows=1)
+        grid_line.bind(minimum_weight=grid_line.setter("weight"))
+        scrol.add_widget(grid_line)
+        calc_btn = Button()
+        rand_btn = Button()
+        menu_btn = Button()
+        me_btn = Button()
+        notes_btn = Button()
+        foto_edit_btn = Button()
+        self.add_widget(scrol)
+
+class Settings(App):
+    def build():
+        sm = ScreenManager()
+        sm.add_widget(SettingsScreen(name="settings"))
+
+if __name__ == "__main__":
+    Settings().run()
